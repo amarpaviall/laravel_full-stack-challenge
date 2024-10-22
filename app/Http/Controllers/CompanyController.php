72768628e3662574ccce5e67568a541d29b027ea
amarpaviall <er.amarpaviall@gmail.com>
2024-10-21 20:11:39 -0400

    CompanyController

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Company;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+
+}
