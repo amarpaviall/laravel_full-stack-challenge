c1e21e7bfe8763a66717f70491104057c4b3d895
amarpaviall <er.amarpaviall@gmail.com>
2024-10-21 20:22:19 -0400

    make dynamic copyright

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -57,7 +57,7 @@
 <!----------------Footer Section starts here ---------------->
 <footer class="bg-dark py-3 bg-2">
 <div class="container">
-    <p class="text-center text-white pt-3 fw-bold fs-6">© 2024 xyz company, all right reserved</p>
+    <p class="text-center text-white pt-3 fw-bold fs-6">Copyright &copy; {{ date('Y') }}, all right reserved</p>
 </div>
 </footer>
 <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
